bad798842190aadf0318ce07977163318e225f64
Eduardo Gomes Heleno <eduardo.gheleno@gmail.com>
2025-10-16 13:39:04 -0300

    migrate mysql VPS instance to dedicated service

--- cmd/message_consumer/main.go
+++ cmd/message_consumer/main.go
@@ -41,7 +41,7 @@ func initDatabase() *gorm.DB {
 		log.Panicf("Password file not found: %s", fileErr)
 	}
 
-	dsn := fmt.Sprintf("root:%s@tcp(chat_database:3306)/chat_api?charset=utf8&parseTime=true", dbPswd)
+	dsn := fmt.Sprintf("eduardo:%s@tcp(10.0.0.215:3306)/chat_api?charset=utf8&parseTime=true", dbPswd)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Panicf("Failed to connect to database: %s", err)
